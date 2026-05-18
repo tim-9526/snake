59975aa6475bfd2fefcdb99e5ecf07d3b1ce9ed8
tim-9526 <loarder001@gmail.com>
2026-05-18 19:44:28 +0800

    chore: 移除密码认证

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import PasswordGate, { isAuthenticated } from './components/PasswordGate.jsx'
 import './styles/global.css'
-
-function Root() {
-  const [passed, setPassed] = useState(() => { try { return isAuthenticated() } catch { return false } })
-  if (!passed) return <PasswordGate onPass={() => setPassed(true)} />
-  return <App />
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
 )
